1ca1f439e216334d0e098b9a17358f9c88dd6e7a
KaanGullum <163915166+KaanGullum@users.noreply.github.com>
2026-05-26 07:31:12 +0300

    Initial Commit

---
+++ InternshipTracker/InternshipTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternshipTrackerApp.swift
+//  InternshipTracker
+//
+//  Created by Kaan Güllü on 26.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct InternshipTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
